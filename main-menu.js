c0c758956e14b4da58978cd3ad0981574d964a70
Matthew Lean <matt@mattlean.com>
2018-10-21 18:29:56 -0700

    Add dialog boxes

--- main-menu.js
+++ main-menu.js
@@ -1,11 +1,32 @@
 const { app, Menu } = require('electron');
+
+const { showMessage, showOpenDialog, showSaveDialog } = require('./dialogs');
+
 const isWindows = process.platform === 'win32';
 
-function setMainMenu() {
+function setMainMenu(mainWindow) {
 	const template = [
 		{
 			label: isWindows ? 'File' : app.getName(),
 			submenu: [
+				{
+					label: 'Say Hello',
+					click() {
+						showMessage(mainWindow);
+					}
+				},
+				{
+					label: 'Save Memory Usage Info',
+					click() {
+						showSaveDialog(mainWindow);
+					}
+				},
+				{
+					label: 'Open File',
+					click() {
+						showOpenDialog(mainWindow);
+					}
+				},
 				{
 					label: isWindows ? 'Exit' : `Quit ${app.getName()}`,
 					accelerator: isWindows ? 'Alt+F4' : 'CmdOrCtrl+Q',
